6881b1848493807ecc34ab0a2f89f46264c49f88
giorgialui <giorgia.lui@galileiostiglia.edu.it>
2026-03-13 09:59:27 +0000

    es23.c

--- es23.c
+++ es23.c
@@ -1,17 +1,20 @@
-/*Leggere in input 10 numeri e calcolare la somma*/
+/*Leggere in input 10 numeri e calcolare la somma e la media*/
 
 #include <stdio.h>
 
 int main() {
     int numero, somma = 0;
+    float media;
+
 
     for(int i = 0; i < 10; i++){
         printf("inserisci un numero: ");
         scanf("%d", &numero);
     somma = somma + numero;
     }
-
+    media = (float)somma / 10;
     printf("la somma dei numeri inseriti è %d \n", somma);
+    printf("la media dei numeri inseriti è %.2f \n", media);
 
 return 0;
 }
