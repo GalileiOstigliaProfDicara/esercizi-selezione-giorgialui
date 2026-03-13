40a2361467a48b0218eb476040972f95c57fe59f
giorgialui <giorgia.lui@galileiostiglia.edu.it>
2026-03-13 09:30:58 +0000

    es22.c

--- es22.c
+++ es22.c
@@ -1,17 +1,21 @@
 /*Scrivere i  numeri naturali compresi tra 1 e un numero letto in input estremi esclusi.*/
+
 #include <stdio.h>
 
 int main() {
-    int num, i;
+    int num;
 
+    do {
     printf("Inserisci un numero: ");
     scanf("%d", &num);
 
-    i = 2;
-
-    while(i < num) {
-        printf("%d ", i);
-        i++;
+    if(num >= 1){
+        printf("inserisci un numero più grande: \n");
+    }
+}
+    while(num <= 1);
+    for (int i = 2; i < num; i++) {
+        printf("%d\n", i);
     }
 
     return 0;
